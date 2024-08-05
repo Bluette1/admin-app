b2963890d9a5fbd08aa12df3e25d9a66dba195e7
Bluette1 <marylene.sawyer@gmail.com>
2024-08-05 20:13:16 +0200

    Add project files

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b7@hmc($$c7^^b#w06mf+l#2e8h_a6e!7r(f#s51_m0-krv(mp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0',]
 
 
 # Application definition
