0010f20200232fb70b196801aaca36e608b48a77
Bluette1 <marylene.sawyer@gmail.com>
2024-08-14 09:20:13 +0200

    Add serializers and views

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b7@hmc($$c7^^b#w06mf+l#2e8h_a6e!7r(f#s51_m0-krv(mp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0',]
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ViewSet):
+  def list(self, request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many = True)
+    return Response(serializer.data)
+
+  def create(self, request):
+    serializer=ProductSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status.HTTP_201_CREATED)
+    
+  def retrieve(self, request, pk=None):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+  def update(self, request, pk=None):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(instance=product, data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status.HTTP_202_ACCEPTED)
+
+
+  def destroy(self, request, pk=None):
+    product = Product.objects.get(id=pk)
+    product.delete()
+    return Response(status.HTTP_204_NO_CONTENT)
+
+
+
+
