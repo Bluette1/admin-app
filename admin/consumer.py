4bf989f9f7fccd0bcf5cfea7129463c4865eca1a
Bluette1 <marylene.sawyer@gmail.com>
2024-08-27 04:41:02 +0200

    Publish to admin app

--- admin/consumer.py
+++ admin/consumer.py
@@ -1,4 +1,9 @@
-import pika
+import pika, json, os, django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin.settings')
+django.setup()
+
+from products.models import Product
 
 params = pika.URLParameters('amqps://airxfsao:W5tmIncTdIhXialDQB-ExXj-jMEBc1Iu@armadillo.rmq.cloudamqp.com/airxfsao')
 
@@ -9,7 +14,12 @@ channel.queue_declare(queue='admin')
 
 def callback(ch, method, properties, body):
   print('Received in admin')
+  id=json.loads(body)
   print(body)
+  product = Product.objects.get(id=id)
+  product.likes = product.likes + 1
+  product.save()
+  print('Product likes increased')
 
 channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
 
